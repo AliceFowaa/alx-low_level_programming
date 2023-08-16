cae4f66b145d51971bdc5f7f56a0227eee28b3b4
Alice Fowaa <alicefowaa094@gmail.com>
2023-08-16 19:06:20 +0000

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - print _putchar
+ * is written by alice
+ * Return: 0
+ */
+int main(void)
+{
+	char putcharvar[10] = "_putchar";
+
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(putcharvar[i]);
+	}
+	_putchar(10);
+	return (0);
+}
